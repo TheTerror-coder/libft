7c69e25c474e6d6c8684664e5ef8748b76208339
TheTerror-coder <johnmrfaye12@gmailcom>
2022-11-20 23:43:21 +0100

    ajout de la fonction ft_substr Il faudra vérifier s'il faut malloc la sous chaine par rapport au nombre  de caractéres qu'il va comporter ou est-ce que 'len' est exactement la taille qu'il faut allouer? En gros faut-il allouer en fonction du nombre de caractére qui reste sur 's' à partir de 'start'

---
+++ ft_substr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 21:20:19 by TheTerror         #+#    #+#             */
+/*   Updated: 2022/11/19 21:20:19 by TheTerror        ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, unsigned int len)
+{
+	char			*ss;
+	unsigned int	i;
+	unsigned int	ss_len;
+
+	i = start;
+	ss_len = 0;
+	while (s[i])
+	{
+		i++;
+		ss_len++;
+	}
+	if (ss_len > len)
+		ss_len = len;
+	ss = malloc(sizeof(char *) * (ss_len + 1));
+	if (!ss)
+		return (0);
+	i = 0;
+	while (i < ss_len)
+	{
+		ss[i] = s[start];
+		i++;
+		start++;
+	}
+	ss[i] = '\0';
+	return (ss);
+}
