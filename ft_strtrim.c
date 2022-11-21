e0d9ed4e1383216f3a47f6eaa0aee1a7dc381488
TheTerror-coder <johnmrfaye12@gmailcom>
2022-11-21 22:04:33 +0100

    Ajout de la fonction ft_strtrim Cette fonction fonctionne correctement mais je ne suis pas certains d'avoir bien compris la consigne Duc oup je refaire une autre version et il faudra vérifier la bonne comprehension du sujet

---
+++ ft_strtrim.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 13:43:18 by TheTerror         #+#    #+#             */
+/*   Updated: 2022/11/21 13:43:18 by TheTerror        ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int	ft_isalnum(int c)
+{
+	if ((c > 47 && c < 58) || (c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	return (0);
+}
+
+int	ft_strlen(const char *s, unsigned int op)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (op != 0)
+	{
+		if (i > 0)
+			i--;
+		while (!ft_isalnum(s[i]))
+			i--;
+	}
+	return (i);
+}
+
+int	ft_is_in_set(const char *set, char c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_fill(char *str, const char *s1, const char *set, int *len)
+{
+	int	i;
+
+	i = 0;
+	while (!ft_isalnum(s1[i]))
+	{
+		if (!ft_is_in_set(set, s1[i]))
+		{
+			str[*len] = s1[i];
+			*len = *len + 1;
+		}
+		i++;
+	}
+	str[*len] = 0;
+	return (i);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		len;
+	int		i;
+	int		k;
+	char	*str;
+
+	i = 0;
+	len = ft_strlen(s1, 0);
+	k = ft_strlen(s1, 1);
+	str = malloc(sizeof(char *) * (len + 1));
+	if (!str)
+		return (0);
+	len = 0;
+	i = ft_fill(str, s1, set, &len);
+	while (i <= k)
+	{
+		str[len] = s1[i];
+		len++;
+		i++;
+	}
+	i = ft_fill(str, &s1[i], set, &len);
+	return (str);
+}
