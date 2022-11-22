baa2897ae45c360776e45c9d330bd207b4f840dd
TheTerror-coder <johnmrfaye12@gmailcom>
2022-11-22 22:23:21 +0100

    Ajout de la fonction ft_strmapi J'ai rien compris des paramétres à passer à la fonction 'f' Donc j'ai pas pu testé le bon fonctionnement de ma fonction Fonction à vérfier!!!!

---
+++ ft_strmapi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 21:19:13 by TheTerror         #+#    #+#             */
+/*   Updated: 2022/11/22 21:19:13 by TheTerror        ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdlib.h>
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int 	len;
+	int 	i;
+
+	i = 0;
+	len = ft_strlen(s);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
