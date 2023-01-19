6cd0f1f62258cc0f4ee7374fbeddd69bcd2876b4
TheTerror-coder <117765114+TheTerror-coder@users.noreply.github.com>
2023-01-19 18:40:54 +0100

    Personalisation des valeurs de retour en fonction des cas

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -19,9 +19,9 @@ int	ft_isalnum(int c)
 		if (c > 47 && c < 58)
 			return (1);
 		if (c > 64 && c < 91)
-			return (1);
+			return (2);
 		if (c > 96 && c < 123)
-			return (1);
+			return (4);
 	}
 	return (0);
 }
