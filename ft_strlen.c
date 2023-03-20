f5c1b37774ea8dfaaaf9f56c0d722b72a8539251
TheTerror <johnmrfaye12@gmail.com>
2023-03-20 23:07:19 +0100

    sécurisation de ft_strlen()

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:32:09 by TheTerror         #+#    #+#             */
-/*   Updated: 2022/12/15 18:59:00 by TheTerror        ###   ########lyon.fr   */
+/*   Updated: 2023/03/20 22:58:41 by TheTerror        ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ unsigned int	ft_strlen(const char *s)
 	unsigned int	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
 		i++;
 	return (i);
